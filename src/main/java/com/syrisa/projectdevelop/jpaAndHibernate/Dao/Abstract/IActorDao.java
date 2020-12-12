6efa6b0cdb9973f61c3eb9bb7444c2cb83869a64
Syr7-s <isasayar7@github.com>
2020-12-12 13:06:29 +0300

    IActorDao interface added to project.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/Abstract/IActorDao.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/Abstract/IActorDao.java
@@ -1,2 +1,13 @@
-package com.syrisa.projectdevelop.jpaAndHibernate.Dao.Abstract;public interface IActorDao {
+package com.syrisa.projectdevelop.jpaAndHibernate.Dao.Abstract;
+
+import com.syrisa.projectdevelop.jpaAndHibernate.Entities.Actor;
+
+import java.util.List;
+
+public interface IActorDao {
+    List<Actor> getAll();
+    Actor getById(int id);
+    void add(Actor actor);
+    void update(Actor actor);
+    void delete(Actor actor);
 }
