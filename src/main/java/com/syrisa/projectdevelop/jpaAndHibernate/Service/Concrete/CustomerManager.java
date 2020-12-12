544d5e4b4d7e0bb82a9f8c4acbfe2fd8bc033ea7
Syr7-s <isasayar7@github.com>
2020-12-12 15:49:51 +0300

    CustomerManager updated and some methods added.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Service/Concrete/CustomerManager.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Service/Concrete/CustomerManager.java
@@ -1,6 +1,7 @@
 package com.syrisa.projectdevelop.jpaAndHibernate.Service.Concrete;
 
 
+import com.syrisa.projectdevelop.jpaAndHibernate.Dao.Abstract.ICustomerDao;
 import com.syrisa.projectdevelop.jpaAndHibernate.Entities.Customer;
 import com.syrisa.projectdevelop.jpaAndHibernate.Service.Abstract.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,39 +12,39 @@ import java.util.List;
 
 @Service
 public class CustomerManager implements ICustomerService<Customer>{
-    private ICustomerService customerService;
+    private ICustomerDao customerDao;
     @Autowired
-    public CustomerManager(ICustomerService customerService){
-        this.customerService=customerService;
+    public CustomerManager(ICustomerDao customerDao){
+        this.customerDao=customerDao;
     }
 
     @Override
     @Transactional
     public List<Customer> getAll() {
-        return null;
+        return this.customerDao.getAll();
     }
 
     @Override
     @Transactional
     public Customer getById(int id) {
-        return null;
+        return (Customer) this.customerDao.getById(id);
     }
 
     @Override
     @Transactional
     public void add(Customer customer) {
-
+        this.customerDao.add(customer);
     }
 
     @Override
     @Transactional
     public void update(Customer customer) {
-
+        this.customerDao.update(customer);
     }
 
     @Override
     @Transactional
     public void delete(Customer customer) {
-
+        this.customerDao.delete(customer);
     }
 }
