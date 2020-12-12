adb7d04f4364d3341da6750e757d79c04211ab3d
Syr7-s <isasayar7@github.com>
2020-12-12 13:08:07 +0300

    Actor.java created in Entities directory.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Entities/Actor.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Entities/Actor.java
@@ -1,2 +1,47 @@
-package com.syrisa.projectdevelop.jpaAndHibernate.Entities;public class Actor {
+package com.syrisa.projectdevelop.jpaAndHibernate.Entities;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="actor")
+public class Actor {
+    @Id
+    @Column(name = "actor_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int actorId;
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+    public Actor(){}
+    public Actor(int actorId, String firstName, String lastName) {
+        this.actorId = actorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(int actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
