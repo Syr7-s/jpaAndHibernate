31d7e354292d58734fb060a7d37f29a562fb421d
Syr7-s <isasayar7@github.com>
2020-12-12 14:50:45 +0300

    IEntityRepository created for all  dao concrete class.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/IEntityRepository.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/IEntityRepository.java
@@ -1,2 +1,11 @@
-package com.syrisa.projectdevelop.jpaAndHibernate.Dao;public interface IEntityRepository {
+package com.syrisa.projectdevelop.jpaAndHibernate.Dao;
+
+import java.util.List;
+
+public interface IEntityRepository<T> {
+    List<T> getAll();
+    T getById(int id);
+    void add(T t);
+    void update(T t);
+    void delete(T t);
 }
