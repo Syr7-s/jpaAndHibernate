73046625c3edf69cf53f6e2f4ef1734345a5239a
Syr7-s <isasayar7@github.com>
2020-12-12 16:05:29 +0300

    Update method created in CustomerController.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/restApi/Controller/CustomerController.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/restApi/Controller/CustomerController.java
@@ -3,9 +3,7 @@ package com.syrisa.projectdevelop.jpaAndHibernate.restApi.Controller;
 import com.syrisa.projectdevelop.jpaAndHibernate.Entities.Customer;
 import com.syrisa.projectdevelop.jpaAndHibernate.Service.Abstract.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,9 @@ public class CustomerController {
     @GetMapping("/customers")
     public List<Customer> getAll(){
         return this.customerService.getAll();
+    }
+    @PostMapping("/customer/update")
+    public void update(@RequestBody Customer customer){
+        this.customerService.update(customer);
     }
 }
