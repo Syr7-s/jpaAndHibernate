acadb48397f90932e0c46d3f70cc89962f1601f7
Syr7-s <isasayar7@github.com>
2020-12-12 13:27:15 +0300

    ActorManager.java created in Service directory and added neccessary methods.

---
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Service/Concrete/ActorManager.java
@@ -0,0 +1,48 @@
+package com.syrisa.projectdevelop.jpaAndHibernate.Service.Concrete;
+
+import com.syrisa.projectdevelop.jpaAndHibernate.Dao.Abstract.IActorDao;
+import com.syrisa.projectdevelop.jpaAndHibernate.Entities.Actor;
+import com.syrisa.projectdevelop.jpaAndHibernate.Service.Abstract.IActorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+public class ActorManager implements IActorService<Actor> {
+    private IActorDao actorDao;
+    //Constructor injection
+    @Autowired
+    public ActorManager(IActorDao actorDao){
+        this.actorDao=actorDao;
+    }
+    @Override
+    @Transactional
+    public List<Actor> getAll() {
+        return this.actorDao.getAll();
+    }
+
+    @Override
+    @Transactional
+    public Actor getById(int id) {
+        return this.actorDao.getById(id);
+    }
+
+    @Override
+    @Transactional
+    public void add(Actor actor) {
+        this.actorDao.add(actor);
+    }
+
+    @Override
+    @Transactional
+    public void update(Actor actor) {
+        this.actorDao.update(actor);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Actor actor) {
+        this.actorDao.delete(actor);
+    }
+}
