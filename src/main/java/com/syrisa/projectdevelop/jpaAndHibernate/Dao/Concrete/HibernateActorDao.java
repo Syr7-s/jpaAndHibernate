a4be4f7cc5b6ec2def43277d52661b02f6c02947
Syr7-s <isasayar7@github.com>
2020-12-12 13:07:14 +0300

    HibernateActorDao class added to project.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/Concrete/HibernateActorDao.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/Concrete/HibernateActorDao.java
@@ -2,14 +2,27 @@ package com.syrisa.projectdevelop.jpaAndHibernate.Dao.Concrete;
 
 import com.syrisa.projectdevelop.jpaAndHibernate.Dao.Abstract.IActorDao;
 import com.syrisa.projectdevelop.jpaAndHibernate.Entities.Actor;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.List;
-
-public class ActorDao implements IActorDao {
-    
+@Repository
+public class HibernateActorDao implements IActorDao {
+    private EntityManager entityManager;
+    @Autowired
+    public HibernateActorDao(EntityManager entityManager){
+        this.entityManager=entityManager;
+    }
     @Override
+    @Transactional
     public List<Actor> getAll() {
-        return null;
+        Session session=entityManager.unwrap(Session.class);
+        List<Actor> actors=session.createQuery("from Actor",Actor.class).getResultList();
+        return actors;
+
     }
 
     @Override
