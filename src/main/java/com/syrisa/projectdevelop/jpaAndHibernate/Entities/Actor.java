e1f9087e6d42027317c93116df412c6af0fd8139
Syr7-s <isasayar7@github.com>
2020-12-12 13:51:33 +0300

    Actor.java updated.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Entities/Actor.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Entities/Actor.java
@@ -10,9 +10,9 @@ public class Actor {
     @Column(name = "actor_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int actorId;
-    @Column(name = "firstname")
+    @Column(name = "first_name")
     private String firstName;
-    @Column(name = "lastname")
+    @Column(name = "last_name")
     private String lastName;
     public Actor(){}
     public Actor(int actorId, String firstName, String lastName) {
