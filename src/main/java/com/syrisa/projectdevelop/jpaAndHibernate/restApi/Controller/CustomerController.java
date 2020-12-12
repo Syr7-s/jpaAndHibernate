f72504e94ead030cb9ac9c8943e6d98efec4a5cb
Syr7-s <isasayar7@github.com>
2020-12-12 16:04:03 +0300

    Add method created in CustomerController.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/restApi/Controller/CustomerController.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/restApi/Controller/CustomerController.java
@@ -3,9 +3,7 @@ package com.syrisa.projectdevelop.jpaAndHibernate.restApi.Controller;
 import com.syrisa.projectdevelop.jpaAndHibernate.Entities.Customer;
 import com.syrisa.projectdevelop.jpaAndHibernate.Service.Abstract.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,13 @@ public class CustomerController {
     @GetMapping("/customers")
     public List<Customer> getAll(){
         return this.customerService.getAll();
+    }
+    @GetMapping("/customers/{id}")
+    public Customer getById(@PathVariable int id){
+        return (Customer) this.customerService.getById(id);
+    }
+    @PostMapping("/customer/add")
+    public void add(@RequestBody Customer customer){
+        this.customerService.add(customer);
     }
 }
