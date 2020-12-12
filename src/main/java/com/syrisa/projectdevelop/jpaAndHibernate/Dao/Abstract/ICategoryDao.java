7429014417c2b50d4ca2a0dcf905d39ee96bf69c
Syr7-s <isasayar7@github.com>
2020-12-12 14:48:40 +0300

    ICategoryDao updated.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/Abstract/ICategoryDao.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/Abstract/ICategoryDao.java
@@ -1,2 +1,7 @@
-package com.syrisa.projectdevelop.jpaAndHibernate.Dao.Abstract;public interface ICategoryDao {
+package com.syrisa.projectdevelop.jpaAndHibernate.Dao.Abstract;
+
+import com.syrisa.projectdevelop.jpaAndHibernate.Dao.IEntityRepository;
+
+public interface ICategoryDao<Category> extends IEntityRepository<Category> {
+
 }
