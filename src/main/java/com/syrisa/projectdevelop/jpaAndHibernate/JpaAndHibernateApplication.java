c4525a56bc324c65b492477aa72ce259fabce4d0
Syr7-s <isasayar7@github.com>
2020-12-12 12:10:10 +0300

    initial commit

---
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/JpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.syrisa.projectdevelop.jpaAndHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syrisa/projectdevelop/jpaAndHibernate/JpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.projectdevelop.jpaAndHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
