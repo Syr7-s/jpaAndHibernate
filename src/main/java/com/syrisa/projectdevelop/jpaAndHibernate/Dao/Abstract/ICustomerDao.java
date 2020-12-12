000e3b01bfd32817b320e7e50bbdbe115a2a9433
Syr7-s <isasayar7@github.com>
2020-12-12 15:29:19 +0300

    CustomerDao and ICustomerService created.

---
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/Dao/Abstract/ICustomerDao.java
@@ -0,0 +1,8 @@
+package com.syrisa.projectdevelop.jpaAndHibernate.Dao.Abstract;
+
+import com.syrisa.projectdevelop.jpaAndHibernate.Dao.IEntityRepository;
+
+
+public interface ICustomerDao extends IEntityRepository {
+
+}
