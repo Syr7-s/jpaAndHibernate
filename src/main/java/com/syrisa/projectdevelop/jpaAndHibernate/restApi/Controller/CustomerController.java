7de962f5d6da29ced14979a95bcb6bd3dbc0a207
Syr7-s <isasayar7@github.com>
2020-12-12 16:00:49 +0300

    CustomerController added to getAll method.

--- src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/restApi/Controller/CustomerController.java
+++ src/main/java/com/syrisa/projectdevelop/jpaAndHibernate/restApi/Controller/CustomerController.java
@@ -1,13 +1,24 @@
 package com.syrisa.projectdevelop.jpaAndHibernate.restApi.Controller;
 
+import com.syrisa.projectdevelop.jpaAndHibernate.Entities.Customer;
 import com.syrisa.projectdevelop.jpaAndHibernate.Service.Abstract.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
     private ICustomerService customerService;
-
+    @Autowired
+    public CustomerController(ICustomerService customerService){
+        this.customerService=customerService;
+    }
+    @GetMapping("/customers")
+    public List<Customer> getAll(){
+        return this.customerService.getAll();
+    }
 }
